f7306bb4951aa03b255d2c12f1e49e75ec5650b7
decodejatin <jatin.bhasin@rooter.io>
2025-05-23 03:12:53 +0530

    change nextjsconfig

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-module.exports = {
   output: "export",
   eslint: {
     ignoreDuringBuilds: true,
