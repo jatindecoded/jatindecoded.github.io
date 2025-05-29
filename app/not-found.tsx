506794d9c1c1863a53c3d822b045922fbc39a749
decodejatin <jatin.bhasin@rooter.io>
2025-05-30 03:18:35 +0530

    add 404 page and other changes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,16 @@
 import Link from 'next/link'
+import properties from "@/data/properties.json"
 
 export default function NotFound() {
+	const bg = properties["media.homepage.photo.1"].media[0];
 	return (
-		<div className='w-full text-center py-32'>
-			<h2 className='text-[36px] font-bold tracking-tight'>Something's Wrong Here</h2>
-			<p className='text-muted-foreground pb-6'>We can't find the page you are looking for.</p>
-			<Link href="/" className='underline text-primary font-bold'>Return Home</Link>
+		<div className='w-full text-center py-32 relative bg-blend-lighten relative'
+			style={{ backgroundImage: `url(${bg})`, backgroundSize: 'contain', backgroundPosition: 'center' }}
+		>
+			<div className="absolute inset-0 bg-white/90" />
+			<h2 className='text-xl md:text-[30px] font-bold tracking-tight relative'>Something's Wrong Here</h2>
+			<p className='text-muted-foreground pb-6 relative'>We can't find the page you are looking for.</p>
+			<Link href="/" className='underline text-primary relative font-bold'>Return Home</Link>
 		</div>
 	)
 }
